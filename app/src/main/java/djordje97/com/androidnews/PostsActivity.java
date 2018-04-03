cb859a898b22925ad2cc24a773b65f02306caa29
Djordje Stevanovic <djordje97.tkd@gmail.com>
2018-04-03 23:14:38 +0200

    Created toolbar

--- app/src/main/java/djordje97/com/androidnews/PostsActivity.java
+++ app/src/main/java/djordje97/com/androidnews/PostsActivity.java
@@ -1,8 +1,13 @@
 package djordje97.com.androidnews;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class PostsActivity extends AppCompatActivity {
@@ -11,6 +16,33 @@ public class PostsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id == R.id.action_settings)
+        {
+            Intent openSettings=new Intent(PostsActivity.this,SettingsActivity.class);
+            startActivity(openSettings);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void StartReadPostActivity(View view) {
--- app/src/main/java/djordje97/com/androidnews/ReadPostActivity.java
+++ app/src/main/java/djordje97/com/androidnews/ReadPostActivity.java
@@ -1,7 +1,12 @@
 package djordje97.com.androidnews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ReadPostActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class ReadPostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_post);
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id == R.id.action_settings)
+        {
+            Intent openSettings=new Intent(ReadPostActivity.this,SettingsActivity.class);
+            startActivity(openSettings);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
